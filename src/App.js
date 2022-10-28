47e257e458b2447c235af9a6a7131bbc3b7b6bc6
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-10-28 14:18:52 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let j = -1;
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((res) => {
+        let imgArr = [];
+        for (let i in res.data.memes) {
+          imgArr.push(res.data.memes[i].url);
+        }
+      });
+  }
+
+  render() {
+    const nextClick = () => {
+      j++
+      if (j > 99) {
+        j = 0
+      }
+      document.getElementById("counter").innerHTML = `${j + 1}/100`
+      fetch("https://api.imgflip.com/get_memes")
+        .then((res) => res.json())
+        .then((res) => {
+          let imgArr = [];
+          for (let i in res.data.memes) {
+            imgArr.push(res.data.memes[i].url);
+          }
+          document.querySelector("#meme").src = imgArr[j];
+        })
+    };
+
+    const backClick = () => {
+      j--
+      if (j < 0) {
+        j = 99
+      }
+      document.getElementById("counter").innerHTML = `${j + 1}/100`
+      fetch("https://api.imgflip.com/get_memes")
+        .then((res) => res.json())
+        .then((res) => {
+          let imgArr = [];
+          for (let i in res.data.memes) {
+            imgArr.push(res.data.memes[i].url);
+          }
+          document.querySelector("#meme").src = imgArr[j];
+        })
+    };
+
+    const randomClick = () => {
+      j = Math.floor(Math.random() * 99)
+      document.getElementById("counter").innerHTML = `${j + 1}/100`
+      fetch("https://api.imgflip.com/get_memes")
+        .then((res) => res.json())
+        .then((res) => {
+          let imgArr = [];
+          for (let i in res.data.memes) {
+            imgArr.push(res.data.memes[i].url);
+          }
+          document.querySelector("#meme").src = imgArr[j];
+        })
+    };
+    return (
+      <div className="App">
+        <h1>Meme Templates!</h1>
+        <h2 onClick={nextClick} id="nextButton">Next</h2>
+        <h2 onClick={randomClick} id="randomButton">Surprise Me!</h2>
+        <h2 onClick={backClick} id="backButton">Back</h2>
+        <h3 id="counter"></h3>
+        <div id="memeCont"><img id="meme" src="" /></div>
+      </div>
+    );
+  }
 }
 
 export default App;
